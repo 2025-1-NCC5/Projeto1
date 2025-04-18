ee300d0bbf907f7a1667eea98926504c6686de24
Biaa099 <133707298+Biaa099@users.noreply.github.com>
2025-04-18 00:30:16 -0300

    MVP - Front #79

--- src/Frontend/pi_5ccomp/lib/screens/home.dart
+++ src/Frontend/pi_5ccomp/lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pi_5ccomp/components/decoration_auth.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,7 +8,69 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber,
+      body: Padding(
+        padding: const EdgeInsets.only(top: 100.0, left: 40.0, right: 40.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Local Inicial",
+                      filled: true,
+                      fillColor: Color.fromARGB(1000, 217, 217, 217),
+                      prefixIcon: Icon(Icons.search),
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 20.0,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Destino Final",
+                      filled: true,
+                      fillColor: Color.fromARGB(1000, 217, 217, 217),
+                      prefixIcon: Icon(Icons.location_pin),
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 20.0,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(1000, 28, 140, 164),
+                  shape: BoxShape.circle,
+                ),
+                child: IconButton(
+                  icon: Icon(Icons.search, color: Colors.white),
+                  onPressed: () {
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
